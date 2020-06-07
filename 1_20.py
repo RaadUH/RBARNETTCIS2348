eff2ccc8556d129e71f61048f5c3a9caec27f0a9
RaadUH <66492134+RaadUH@users.noreply.github.com>
2020-06-07 15:05:31 -0500

    Add files via upload

---
+++ 1_20.py
@@ -0,0 +1,9 @@
+user_num = int(input('Enter integer:\n'))
+
+# Type your code here
+print("You entered:",user_num)
+print("{0} squared is {1}".format(user_num,user_num**2))
+print("And {0} cubed is {1} !!".format(user_num,user_num**3))
+user_num2 = int(input("Enter another integer:\n"))
+print("{0} + {1} is {2}".format(user_num,user_num2,user_num+user_num2))
+print("{0} * {1} is {2}".format(user_num,user_num2,user_num*user_num2))
