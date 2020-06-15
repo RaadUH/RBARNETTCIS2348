771b1534767ee6723006f4af64892387b1e096f1
RaadUH <66492134+RaadUH@users.noreply.github.com>
2020-06-15 11:58:09 -0500

    Add files via upload

---
+++ homework_2a.py
@@ -0,0 +1,50 @@
+def find(date):
+    output = ''
+    list1 = date.split(' ')
+    if(list1[0]=='January'):
+         output+='1'
+    elif(list1[0]=='Feburary'):
+       output+='2'
+    elif(list1[0]=='March'):
+       output+='3'
+    elif(list1[0]=='April'):
+       output+='4'
+    elif(list1[0]=='May'):
+       output+='5'
+    elif(list1[0]=='June'):
+       output+='6'
+    elif(list1[0]=='July'):
+        output+='7'
+    elif(list1[0]=='August'):
+        output+='8'
+    elif(list1[0]=='September'):
+        output+='9'
+    elif(list1[0]=='October'):
+        output+='10'
+    elif(list1[0]=='November'):
+        output+='11'
+    elif(list1[0]=='December'):
+        output+='12'
+    output+='/'
+    output+=list1[1][:-1]
+    output+='/'
+    output+=list1[2]
+    return output
+flag = 0
+list3 = []
+while(flag==0):
+    input1 = input()
+    if(input1=='-1'):
+        flag=1
+    else:
+        list1 = ['January','February','March','April','May','June','July','August','September','October','November','December']
+        list2 = input1.split(' ')
+        if(list2[0] not in list1):
+            continue
+        elif(list2[1][-1]!=',' or int(list2[1][:-1])>31 or int(list2[1][:-1])<1):
+            continue
+        else:
+            list3.append(find(input1))
+for date in list3:
+    print(date)
+        
