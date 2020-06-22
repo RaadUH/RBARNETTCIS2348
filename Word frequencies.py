4353fe410ed4470fbce35696f51e07b7246faff3
RaadUH <66492134+RaadUH@users.noreply.github.com>
2020-06-22 11:29:27 -0500

    Add files via upload

---
+++ Word frequencies.py
@@ -0,0 +1,9 @@
+list1 = input().split()
+dict1 = dict()
+for word in list1:
+    if(word in dict1.keys()):
+        dict1[word]+=1
+    else:
+        dict1[word]=1
+for word in list1:
+    print(word,dict1[word])
